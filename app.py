fc986c8989e4d7ffacfe2d18d54cd14d98115862
Gaurang <gaurangjadhav045@gmail.com>
2025-08-28 13:12:35 +0530

    uploading

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def display():
+    message = "Hello, this is your display page!"
+    return render_template('display.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
